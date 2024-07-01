09c302b198e9fe9799aa4e791f7c32842c1412e2
Rashed Suwaidi <x.suwaidi@gmail.com>
2024-07-01 13:30:01 +0400

    add an empty menu page

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -8,6 +8,8 @@ import {
   IonButtons,
   IonButton,
   IonIcon,
+  IonMenu,
+  IonMenuToggle,
 } from "@ionic/react";
 import Editor from "../../components/Editor";
 import { useEffect, useState } from "react";
@@ -32,48 +34,63 @@ export default function EditorPage() {
   }, []);
 
   return (
-    <IonPage>
-      <IonHeader translucent={false}>
-        <IonToolbar>
-          <IonButtons slot="secondary">
-            <IonButton>
-              <IonIcon
-                className=" text-gray-300"
-                slot="icon-only"
-                icon={chevronBack}
-              ></IonIcon>
-            </IonButton>
-          </IonButtons>
-          <IonTitle>{title}</IonTitle>
-          <IonButtons slot="primary">
-            <IonButton>
-              <IonIcon
-                className=" text-gray-300"
-                slot="icon-only"
-                icon={ellipsisVertical}
-              ></IonIcon>
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent scrollY={false} fullscreen={false}>
-        <div
-          className="ion-content-scroll-host  overflow-auto overscroll-contain"
-          style={{ maxHeight }}
-        >
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonInput
-                className="ml-3 text-3xl font-extrabold"
-                value={title}
-                placeholder="Enter Title"
-                onIonChange={(e) => setTitle(e.detail.value!)}
-              />
-            </IonToolbar>
-          </IonHeader>
-          <Editor />
-        </div>
-      </IonContent>
-    </IonPage>
+    <>
+      <IonMenu contentId="main-content" type="push">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          This is the menu content.
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader translucent={false}>
+          <IonToolbar>
+            <IonButtons slot="secondary">
+              <IonMenuToggle>
+                <IonButton>
+                  <IonIcon
+                    className=" text-gray-300"
+                    slot="icon-only"
+                    icon={chevronBack}
+                  ></IonIcon>
+                </IonButton>
+              </IonMenuToggle>
+            </IonButtons>
+            <IonTitle>{title}</IonTitle>
+            <IonButtons slot="primary">
+              <IonButton>
+                <IonIcon
+                  className=" text-gray-300"
+                  slot="icon-only"
+                  size="small"
+                  icon={ellipsisVertical}
+                ></IonIcon>
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent scrollY={false} fullscreen={false}>
+          <div
+            className="ion-content-scroll-host  overflow-auto overscroll-contain"
+            style={{ maxHeight }}
+          >
+            <IonHeader collapse="condense">
+              <IonToolbar>
+                <IonInput
+                  className="ml-3 text-3xl font-extrabold"
+                  value={title}
+                  placeholder="Enter Title"
+                  onIonChange={(e) => setTitle(e.detail.value!)}
+                />
+              </IonToolbar>
+            </IonHeader>
+            <Editor />
+          </div>
+        </IonContent>
+      </IonPage>
+    </>
   );
 }
