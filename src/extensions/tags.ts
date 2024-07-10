53729f6b9d3e1b72ec394b3d02f32756273de519
Rashed Suwaidi <x.suwaidi@gmail.com>
2024-07-10 08:02:11 +0400

    add hashtag markdown parse and render

--- src/extensions/tags.ts
+++ src/extensions/tags.ts
@@ -4,6 +4,9 @@ import {
   markPasteRule,
   mergeAttributes,
 } from "@tiptap/core";
+import markdownitHashtag from "markdown-it-hashtag";
+import { PluginSimple } from "markdown-it";
+import MarkdownIt from "markdown-it";
 
 export interface TagsOptions {
   HTMLAttributes: Record<string, any>;
@@ -96,10 +99,33 @@ export const Tags = Mark.create<TagsOptions>({
     return {
       markdown: {
         serialize(state: any, mark: any) {
-          state.write(` #${mark.attrs.tag}`);
+          state.write(`#${mark.attrs.tag} `);
           state.closeMark(mark);
+        },
+        parse: {
+          setup(markdownit: MarkdownIt) {
+            markdownit.use(markdownitHashtag as PluginSimple, {
+              hashtagRegExp: "\\w+",
+              preceding: "^|\\s",
+            });
+
+            markdownit.renderer.rules.hashtag_open = (tokens, idx) => {
+              const tagName = tokens[idx].content.toLowerCase();
+              return `<a href="/tags/${tagName}" class="inline-flex items-center rounded-md bg-gray-50 dark:bg-gray-800 no-underline px-1 py-0.5 text-xs font-medium text-gray-600 dark:text-gray-300 ring-1 ring-inset ring-gray-500/10 dark:ring-gray-500/20">`;
+            };
+
+            markdownit.renderer.rules.hashtag_text = (tokens, idx) => {
+              return `#${tokens[idx].content}`;
+            };
+
+            markdownit.renderer.rules.hashtag_close = () => {
+              return "</a>";
+            };
+          },
         },
       },
     };
   },
 });
+
+export default Tags;
---
+++ src/types/markdown-it-hashtag.d.ts
@@ -0,0 +1,4 @@
+declare module "markdown-it-hashtag" {
+  const markdownitHashtag: any;
+  export default markdownitHashtag;
+}
