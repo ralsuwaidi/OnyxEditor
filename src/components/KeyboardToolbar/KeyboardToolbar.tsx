041517ce81071ee65b8cd1aadc84542599bcc163
Rashed Suwaidi <x.suwaidi@gmail.com>
2024-07-15 11:03:46 +0400

    add header and sliding option

--- src/components/KeyboardToolbar/KeyboardToolbar.tsx
+++ src/components/KeyboardToolbar/KeyboardToolbar.tsx
@@ -9,8 +9,13 @@ import {
   TextItalic,
   Highlighter,
   Link,
+  TextHOne,
+  TextHTwo,
+  TextHThree,
+  TextHFour,
 } from "@phosphor-icons/react";
 import useNoteStore from "../../contexts/noteStore";
+import { Level } from "@tiptap/extension-heading";
 
 const KeyboardToolbar = () => {
   const editor = useNoteStore((state) => state.editor);
@@ -21,6 +26,10 @@ const KeyboardToolbar = () => {
   const [canOutdent, setCanOutdent] = useState(false);
   const [isListItemOrTaskItem, setIsListItemOrTaskItem] = useState(false);
   const [isTextSelected, setIsTextSelected] = useState(false);
+  const [currentHeaderLevel, setCurrentHeaderLevel] = useState<Level | null>(
+    null
+  );
+  const [isHeader, setIsHeader] = useState(false);
 
   useEffect(() => {
     if (editor) {
@@ -39,6 +48,20 @@ const KeyboardToolbar = () => {
           editor.isActive("listItem") || editor.isActive("taskItem")
         );
         setIsTextSelected(editor.state.selection.empty === false);
+
+        // Check for header level
+        const levels: Level[] = [1, 2, 3, 4];
+        let foundHeader = false;
+        for (const level of levels) {
+          if (editor.isActive("heading", { level })) {
+            setCurrentHeaderLevel(level);
+            foundHeader = true;
+          }
+        }
+        setIsHeader(foundHeader);
+        if (!foundHeader) {
+          setCurrentHeaderLevel(null);
+        }
       };
 
       updateToolbar();
@@ -108,43 +131,68 @@ const KeyboardToolbar = () => {
     }
   };
 
+  const handleHeader = (level: Level) => {
+    if (editor) {
+      editor.chain().focus().toggleHeading({ level }).run();
+    }
+  };
+
   return (
-    <div className="absolute inset-x-0 dark:bg-[#2B2B2B] bg-[#CACDD2] bottom-0 flex justify-between items-center px-4">
-      <div className="flex space-x-4 py-2">
-        <ArrowBendUpLeft
-          className={canUndo ? "" : "text-gray-400 dark:text-gray-600"}
-          onClick={handleUndo}
-          size={24}
-        />
-        <ArrowBendUpRight
-          className={canRedo ? "" : "text-gray-400 dark:text-gray-600"}
-          onClick={handleRedo}
-          size={24}
-        />
-        {isListItemOrTaskItem && (
-          <>
-            <TextIndent
-              className={canIndent ? "" : "text-gray-400 dark:text-gray-600"}
-              onClick={handleIndent}
-              size={24}
-            />
-            <TextOutdent
-              className={canOutdent ? "" : "text-gray-400 dark:text-gray-600"}
-              onClick={handleOutdent}
-              size={24}
-            />
-          </>
-        )}
-        {isTextSelected && (
-          <>
-            <TextB onClick={handleBold} size={24} />
-            <TextItalic onClick={handleItalic} size={24} />
-            <Highlighter onClick={handleHighlight} size={24} />
-            <Link onClick={handleLink} size={24} />
-          </>
-        )}
+    <div className="absolute inset-x-0 dark:bg-[#2B2B2B] bg-[#CACDD2] bottom-0 flex justify-between items-center">
+      <div className="flex-1 overflow-x-auto scrollbar-hide">
+        <div className="flex space-x-4 py-2 px-4 min-w-max">
+          <ArrowBendUpLeft
+            className={canUndo ? "" : "text-gray-400 dark:text-gray-600"}
+            onClick={handleUndo}
+            size={24}
+          />
+          <ArrowBendUpRight
+            className={canRedo ? "" : "text-gray-400 dark:text-gray-600"}
+            onClick={handleRedo}
+            size={24}
+          />
+          {isListItemOrTaskItem && (
+            <>
+              <TextIndent
+                className={canIndent ? "" : "text-gray-400 dark:text-gray-600"}
+                onClick={handleIndent}
+                size={24}
+              />
+              <TextOutdent
+                className={canOutdent ? "" : "text-gray-400 dark:text-gray-600"}
+                onClick={handleOutdent}
+                size={24}
+              />
+            </>
+          )}
+          {isTextSelected && (
+            <>
+              <TextB onClick={handleBold} size={24} />
+              <TextItalic onClick={handleItalic} size={24} />
+              <Highlighter onClick={handleHighlight} size={24} />
+              <Link onClick={handleLink} size={24} />
+            </>
+          )}
+          {isHeader &&
+            [1, 2, 3, 4].map((level) => (
+              <button
+                key={level}
+                onClick={() => handleHeader(level as Level)}
+                className={`${
+                  currentHeaderLevel === level
+                    ? "text-gray-400 dark:text-gray-600"
+                    : ""
+                }`}
+              >
+                {level === 1 && <TextHOne size={24} />}
+                {level === 2 && <TextHTwo size={24} />}
+                {level === 3 && <TextHThree size={24} />}
+                {level === 4 && <TextHFour size={24} />}
+              </button>
+            ))}
+        </div>
       </div>
-      <div className="flex items-center justify-end space-x-4 py-2">
+      <div className="flex-shrink-0 py-2 px-4">
         <X size={24} />
       </div>
     </div>
